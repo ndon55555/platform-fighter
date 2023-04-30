297745d957a249468e91a2ee90b33cdc57d9e71b
Don Nguyen <ndon55555@gmail.com>
2023-04-30 15:21:55 -0400

    Add recommended typescript linting and remove semicolons

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,21 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+    },
     plugins: ['@typescript-eslint'],
     root: true,
     env: {
       node: true
+    },
+    ignorePatterns: ['.eslintrc.cjs'],
+    rules: {
+      semi: ['error', 'never']
     }
   };
